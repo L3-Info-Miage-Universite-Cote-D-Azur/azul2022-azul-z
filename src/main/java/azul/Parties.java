67d4c1887c77b37933493c98316a68e25d452506
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2022-09-27 13:51:19 +0200

    lancer 500 parties #10

--- src/main/java/azul/Parties.java
+++ src/main/java/azul/Parties.java
@@ -5,8 +5,11 @@ import azul.moteurdejeu.Azul;
 
 public class Parties {
 
+    Azul partie;
+
     public Parties() {
-        Azul partie = new Azul();
+        partie = new Azul();
+
         Joueur j1 = new Joueur("j1");
         Joueur j2 = new Joueur("j2");
         Joueur j3 = new Joueur("j3");
@@ -14,10 +17,19 @@ public class Parties {
         partie.ajouterJoueur(j1);
         partie.ajouterJoueur(j2);
         partie.ajouterJoueur(j3);
+    }
 
+
+    private void lancer(int nbParties) {
+        for(int i = 0; i < nbParties; i++) {
+            partie.initialisation();
+            partie.jouer();
+        }
     }
 
     public static final void main(String [] args) {
         Parties toutesLesParties = new Parties();
+        toutesLesParties.lancer(500);
     }
+
 }
--- src/main/java/azul/moteurdejeu/Azul.java
+++ src/main/java/azul/moteurdejeu/Azul.java
@@ -5,9 +5,21 @@ import azul.joueur.Joueur;
 import java.util.ArrayList;
 
 public class Azul {
-    private ArrayList<Joueur> listeJoueurs;
+    private ArrayList<Joueur> listeJoueurs = new ArrayList<>();
 
     public void ajouterJoueur(Joueur j) {
         this.listeJoueurs.add(j);
+    }
+
+    /**
+     * remet le jeu à l'état initial
+     */
+    public void initialisation() {
+    }
+
+    /**
+     * dérouler d'une partie
+     */
+    public void jouer() {
     }
 }
